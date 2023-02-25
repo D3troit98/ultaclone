8a83d8e537cd33cfd5672f5143e32c146f5893a4
Detroit98 <duruakuebuka@gmail.com>
2023-02-25 17:58:24 +0100

    updates with animation and ui

--- src/components/Commitments.tsx
+++ src/components/Commitments.tsx
@@ -1,7 +1,17 @@
 import React from 'react'
 
 const Commitments = () => {
-  return <div>Commitments</div>;
+  return (
+    <div className="flex flex-col items-center bg-[#ffffff] w-full  px-20 pt-20">
+      <div className="flex justify-start items-center">
+        <h1 className="font-circular-std-Black text-2xl font-medium">
+          Our Corperate commitments
+        </h1>
+      </div>
+
+      <div className="flex flex-wrap justify-evenly items-start"></div>
+    </div>
+  );
 }
 
 export default Commitments
